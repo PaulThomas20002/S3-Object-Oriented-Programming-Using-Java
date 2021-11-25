9d2b3b6a69ac7227afb9b907e094405fc98c619f
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-11-26 05:05:50 +0530

    myfirstprogram.java file added and commented

---
+++ myfirstprogram.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class myfirstprogram{    //class name should be same as filename
+
+    public static void main( String[] args){
+        System.out.println("My very first Program ... ");     //Just print "My very first Program ... " in a newline
+                                                              //in newline because we used println
+    }
+}
