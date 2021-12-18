3bfc409b746f8c4e299bbc4c424e3c4192729d53
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-18 14:48:58 +0530

    Update AreaParaConstructor.java

--- Mod2/AreaParaConstructor.java
+++ Mod2/AreaParaConstructor.java
@@ -1,7 +1,9 @@
+/*Create a java program to create a class have 3 attribute height , width and length double type. 
+create a method and calculate the volume of box using method volume.*/
 class box
 {
     double width, height, length;
-    box(double a, double b, double c)
+    void setdim(double a, double b, double c)
     {
         width = a;
         height = b;
@@ -17,6 +19,7 @@ class box
      public static void main(String []args)
      {
          box b = new box(10.25, 11.55, 25.25);
+         b.setdim(10.25, 11.55, 25.25);
          double vol = b.volume();
          System.out.println(vol);
      }
