25064879a71087f170346ffa74faad75c1c44b6d
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-18 15:13:45 +0530

    Question updated

--- CSL-203-OOPS-Lab/Cycle2/Class1.java
+++ CSL-203-OOPS-Lab/Cycle2/Class1.java
@@ -1,1 +1,6 @@
-loadin...
+/*   Write a java program which creates a class named employee and have following member name, date, phone number, address, salary .
+It also has a method to print salary which prints the salary which prints the salary of employee.
+2 classes offices and manager have data members specialization and department.
+Now assigb name , age, Phone number, address and salary to an officer and a manager .
+by making an object of both of this classes and print the same.*/
+
